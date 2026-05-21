522f1e616065bb6186678955738cec51b235dd23
NAVEENKUMAR-MANI-200702 <163437596+NAVEENKUMAR-MANI-200702@users.noreply.github.com>
2026-05-21 23:56:03 +0530

    db config

--- backend/api/src/server.js
+++ backend/api/src/server.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+const apiRoutes = require("./routes");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("DailyRoutes API Running");
+  res.json({
+    success: true,
+    message: "DailyRoutes API Running",
+  });
 });
 
-const PORT = 5000;
+app.use("/api/v1", apiRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
